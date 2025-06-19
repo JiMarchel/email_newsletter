598e851d59f8bd223ee8f812f087653946a3508b
JiMarchel <jmarchel100@gmail.com>
2025-06-19 20:58:31 +0700

    main

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
 use email_newsletter::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
-use sqlx::postgres::PgPoolOptions;
-
-use tokio::net::TcpListener;
-
 #[tokio::main]
 async fn main() {
     let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration!");
-    let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect_lazy_with(configuration.database.with_db());
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let addr = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(addr).await.unwrap();
-    run(listener, pool, email_client).await;
+    let application = Application::build(configuration)
+        .await
+        .expect("Failed to build application");
+    application.run_until_stopped().await;
 }
